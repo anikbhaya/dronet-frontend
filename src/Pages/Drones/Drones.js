2e22d1c8062dfef002de5ba03d412442e263d9e7
anikbhaya <mushfiq42bd@gmail.com>
2021-11-13 21:53:04 +0600

    readme updated

--- src/Pages/Drones/Drones.js
+++ src/Pages/Drones/Drones.js
@@ -19,6 +19,22 @@ const Drones = () => {
         }
     },[isLoading])
 
+    if (isLoading) {
+      return (
+          <div className="flex justify-center items-center my-40">
+              <div
+                  className="
+              animate-spin
+              rounded-full
+              h-32
+              w-32
+              border-t-2 border-b-2 border-purple-500
+            "
+              ></div>
+          </div>
+      )
+  }
+
     return (
         <>
         <Header></Header>
